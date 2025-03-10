c8e106abe504c7ef58d27a0fe2c89c76dc15a73b
BrandonHS <branditis1211@gmail.com>
2025-03-10 17:43:43 -0600

    Correccion

--- Formulario/formulario/app/page.tsx
+++ Formulario/formulario/app/page.tsx
@@ -1,95 +1,223 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import React, { useState } from "react";
+import {
+  Box,
+  Container,
+  Typography,
+  useTheme,
+  TextField,
+  Button,
+  FormControl,
+  FormLabel,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+
+import axios from 'axios';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const theme = useTheme();
+  const [formData, setFormData] = useState({
+    nombre: "",
+    puesto: "",
+    ua: "",
+    id: "",
+    extension: "",
+    correo: "",
+    marca: "",
+    modelo: "",
+    serie: "",
+    macadress: "",
+    jefe: "",
+    puestojefe: "",
+    servicios: "",
+    justificacion: ""
+  });
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [pdfUrl, setPdfUrl] = useState(null);
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    try {
+  
+      // Generar PDF
+      const pdfResponse = await axios.post("http://127.0.0.1:3001/api/v1/generar-pdf", formData, {
+        responseType: "blob",
+      });
+  
+      if (pdfResponse.status === 200) {
+        setPdfUrl(URL.createObjectURL(pdfResponse.data));
+      } else {
+        console.error("Error generating PDF");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <Container disableGutters maxWidth="xxl" sx={{background: "#C3FFFE"}}>
+      <Box
+        component="section"
+        sx={{
+          mx: "auto",
+          width: 700,
+          border: "2px solid grey",
+          mt: 8,
+          mb: 8,
+          p: 3,
+          borderRadius: 2,
+          background: "#FFFFFF"
+        }}
+      >
+        <Typography variant="h5" align="center" gutterBottom>
+          Formulario de Registro 
+        </Typography>
+        <Box
+          component="form"
+          sx={{ "& .MuiTextField-root": { mt: 2, width: "100%" } }}
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+
+          <TextField
+            required
+            id="nombre"
+            name="nombre"
+            label="Nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="puesto"
+            name="puesto"
+            label="Puesto"
+            value={formData.puesto}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="ua"
+            name="ua"
+            label="UA"
+            value={formData.ua}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="id"
+            name="id"
+            label="ID"
+            value={formData.id}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="extension"
+            name="extension"
+            label="Extension"
+            value={formData.extension}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="correo"
+            name="correo"
+            label="Correo Electrónico"
+            type="email"
+            value={formData.correo}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="marca"
+            name="marca"
+            label="Marca"
+            value={formData.marca}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="modelo"
+            name="modelo"
+            label="Modelo"
+            value={formData.modelo}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="serie"
+            name="serie"
+            label="Serie"
+            value={formData.serie}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="macadress"
+            name="macadress"
+            label="MACADRESS"
+            value={formData.macadress}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="jefe"
+            name="jefe"
+            label="Nombre del Jefe"
+            value={formData.jefe}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="puestojefe"
+            name="puestojefe"
+            label="Puesto del Jefe"
+            value={formData.puestojefe}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="servicios"
+            name="servicios"
+            label="Servicios"
+            value={formData.servicios}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="justificacion"
+            name="justificacion"
+            label="Justificacion"
+            value={formData.justificacion}
+            onChange={handleChange}
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ mt: 3, width: "100%" }}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            Enviar
+          </Button>
+          {pdfUrl && (
+            <Button variant="outlined" sx={{ mt: 2, width: "100%" }} href={pdfUrl} download="registro.pdf">Descargar PDF</Button>
+          )}
+        </Box>
+      </Box>
+    </Container>
   );
 }
